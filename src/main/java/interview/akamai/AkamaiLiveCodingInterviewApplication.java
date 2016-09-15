7d73f7331cefea689046c5ec35411da94f0c4803
Maksym Novoseltsev <max1m2111@gmail.com>
2016-09-15 08:53:05 +0200

    initial commit

---
+++ src/main/java/interview/akamai/AkamaiLiveCodingInterviewApplication.java
@@ -0,0 +1,12 @@
+package interview.akamai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkamaiLiveCodingInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkamaiLiveCodingInterviewApplication.class, args);
+	}
+}
