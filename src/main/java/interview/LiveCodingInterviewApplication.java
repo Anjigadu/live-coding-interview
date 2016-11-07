cc1b91ae2274ea55cb1a49eea0cab952cb9c250a
Maksym Novoseltsev <max1m2111@gmail.com>
2016-11-07 23:21:04 +0100

    naming fixes

--- src/main/java/interview/LiveCodingInterviewApplication.java
+++ src/main/java/interview/LiveCodingInterviewApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AkamaiLiveCodingInterviewApplication {
+public class LiveCodingInterviewApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AkamaiLiveCodingInterviewApplication.class, args);
+		SpringApplication.run(LiveCodingInterviewApplication.class, args);
 	}
 }
--- src/test/java/interview/LiveCodingInterviewApplicationTests.java
+++ src/test/java/interview/LiveCodingInterviewApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AkamaiLiveCodingInterviewApplicationTests {
+public class LiveCodingInterviewApplicationTests {
 
 	@Test
 	public void contextLoads() {
